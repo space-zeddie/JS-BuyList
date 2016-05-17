d88813b36b0cd4dff509907c508fc6d68491dd8e
space-zeddie <thespacezeddie@gmail.com>
2016-05-17 15:11:42 +0300

    added script.js with sorta working add function

---
+++ script.js
@@ -0,0 +1,40 @@
+$(function () {
+    var ITEM_TEMPLATE =
+        '<div class="buy-item row">' +
+                '<div class="half-row">' +
+                    '<div class="name"></div>' +
+                    '<span class="group center">' +
+                        '<button class="dec">-</button>' +
+                        '<input type="text" name="num-of-items" value="1" readonly>' +
+                        '<button class="inc">+</button>' +
+                    '</span>' +
+                '</div>' +
+                '<span class="group right">' +
+                    '<button class="bought-btn">Куплено</button>' +
+                    '<button class="remove-btn">x</button>' +
+                '</span>' +
+        '</div>';
+    
+    var $buylist = $('.buylist');
+    var $addBtn = $('input[type="submit"]');
+    var $textField = $('input[name="item-name"]');
+    
+    function addItem(name)
+    {
+        var $node = $(ITEM_TEMPLATE);
+        var $name = $node.find('.name');
+        
+        console.log(name);
+        
+        $name.val(name);
+        
+        // TODO add delete logic here
+        
+        $buylist.append($node);
+    }
+    
+    $addBtn.click(function (e) {
+        e.preventDefault();
+        addItem($textField.val());
+    });
+});
